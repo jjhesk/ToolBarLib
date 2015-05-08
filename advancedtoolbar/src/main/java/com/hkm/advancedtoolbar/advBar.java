bd717503c128d3afda796706c7f214845484aa6c
世外桃源 <jobhesk@gmail.com>
2015-05-08 16:37:01 +0800

    test one

--- advancedtoolbar/src/main/java/com/hkm/advancedtoolbar/advBar.java
+++ advancedtoolbar/src/main/java/com/hkm/advancedtoolbar/advBar.java
@@ -1,5 +1,6 @@
 package com.hkm.advancedtoolbar;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
@@ -9,17 +10,32 @@ import android.util.AttributeSet;
  * Created by hesk on 8/5/15.
  */
 public class advBar extends Toolbar {
+    private final Context contx;
+
     public advBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        contx = context;
+        init();
+
     }
 
     public advBar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        contx = context;
+        init();
     }
 
     public advBar(Context context) {
         super(context);
+        contx = context;
+        init();
     }
 
+    private void init() {
 
+    }
+
+    public void colorize(int colorResId) {
+        ToolbarColorizeHelper.colorizeToolbar(this, colorResId, (Activity) contx);
+    }
 }
--- app/src/main/java/com/hkm/toolbarlib/MainActivity.java
+++ app/src/main/java/com/hkm/toolbarlib/MainActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.hkm.advancedtoolbar.advBar;
+
 
 public class MainActivity extends ActionBarActivity {
+    private advBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (advBar) findViewById(R.id.toolbar);
+        toolbar.colorize(R.color.red_300);
+        setSupportActionBar(toolbar);
     }
 
 
